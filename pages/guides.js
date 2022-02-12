54329dd36a8615280948b0f0aba7ce39d1789c42
Susanta <inbox.susanta@gmail.com>
2022-02-12 15:16:45 +0530

    Client side content gurd

--- pages/guides.js
+++ pages/guides.js
@@ -1,11 +1,12 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import AuthContext from '../stores/authContext';
 import styles from '../styles/Guides.module.css';
 
 export default function Guides() {
   const { user, authReady } = useContext(AuthContext);
-
-  console.log(user);
+  // console.log(user);
+  const [guides, setGuides] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     if (authReady) {
@@ -17,14 +18,46 @@ export default function Guides() {
           },
         }
       )
-        .then((res) => res.json())
-        .then((data) => console.log(data));
+        .then((res) => {
+          // console.log(res);
+          if (!res.ok) {
+            throw Error('You must be logged in to view this content');
+          }
+          return res.json();
+        })
+        .then((data) => {
+          // console.log(data)
+          setGuides(data);
+          setError(null);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setGuides(null);
+        });
     }
   }, [user, authReady]);
 
   return (
     <div className={styles.guides}>
-      <h2>All Guides</h2>
+      {!authReady && <div>Loading...</div>}
+
+      {error && (
+        <div className={styles.error}>
+          <p>{error}</p>
+        </div>
+      )}
+
+      {guides &&
+        guides.map((guide) => (
+          <div key={guide.title} className={styles.card}>
+            <h3>{guide.title}</h3>
+            <h4>Written by {guide.author}</h4>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia
+              quisquam minima accusamus, commodi provident sequi?
+            </p>
+          </div>
+        ))}
     </div>
   );
 }
